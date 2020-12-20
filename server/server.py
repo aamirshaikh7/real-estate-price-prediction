3dea29edb44c3fe90c3f44936dd3dfa27f211f8b
shoaib-bilal <68638339+shoaib-bilal@users.noreply.github.com>
2020-12-20 16:52:08 +0530

    setting up file structure and creating artifacts

---
+++ server/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello() :
+    return "Hi"
+
+if __name__ == "__main__" :
+    print("Starting Flask Server for Home Price Prediction")
+    app.run()
