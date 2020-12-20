d069bb4fd8bea33ecbb18253c9578a2001d2b5f4
aamirshaikh7 <aamirshaikh3232@gmail.com>
2020-12-20 17:53:39 +0530

    calculation estimated price

--- server/server.py
+++ server/server.py
@@ -8,6 +8,27 @@ def get_location_names() :
     response = jsonify({
         'locations': util.get_location_names()
     })
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict-price', methods = ['POST'])
+def predict_price() :
+    location = request.form['location']
+    area = float(request.form['area'])
+    bhk = int(request.form['bhk'])
+    gym = int(request.form['gym'])
+    lift = int(request.form['lift'])
+    car_parking = int(request.form['car_parking'])
+    security = int(request.form['security'])
+    playground = int(request.form['playground'])
+    swimming_pool = int(request.form['swimming_pool'])
+
+    response = jsonify({
+        'estimate_price': util.get_estimated_price(location, area, bhk, gym, lift, car_parking, security, playground, swimming_pool)
+    })
+
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
