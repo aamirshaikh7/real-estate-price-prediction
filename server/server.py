26b82a78d231466cf9e3b47b356e081146688b38
aamirshaikh7 <aamirshaikh3232@gmail.com>
2020-12-20 17:07:53 +0530

    Setting up file structure and creating artifacts

--- server/server.py
+++ server/server.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify
+import util
+
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello() :
-    return "Hi"
+@app.route('/get-location')
+def get_location_names() :
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 if __name__ == "__main__" :
     print("Starting Flask Server for Home Price Prediction")
