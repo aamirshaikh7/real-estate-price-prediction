b09eea3501360b11ae272c0a4cced914a62ac9ec
aamirshaikh7 <aamirshaikh3232@gmail.com>
2020-12-20 17:21:39 +0530

    loading artifacts

--- server/util.py
+++ server/util.py
@@ -1,5 +1,26 @@
+import json
+import pickle
+
+__locations = None
+__data_columns = None
+__model = None
+
 def get_location_names() :
-    pass
+    return __locations
+
+def load_saved_artifacts() :
+    print("loading saved artifacts..")
+    global __data_columns
+    global __locations
+
+    with open("./artifacts/columns2.json", 'r') as f :
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[8:]
+
+    with open("./artifacts/mumbai_home_prices_model.pickle", 'rb') as f :
+        __model = pickle.load(f)
+    print("Done loading artifacts..")
 
 if __name__ == '__main__' :
+    load_saved_artifacts()
     print(get_location_names())
